606848bb691cee9165df1a46f4f345706d776cfc
Harun Davood <davoodharun@gmail.com>
2016-08-16 12:35:48 -0400

    removed admin model

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -11,19 +11,14 @@ var UserSchema = new mongoose.Schema({
     unique: true
   },
 
-  firstName: {
-    type: String,
-    required: false
-  },
-
-  lastName: {
-    type: String,
-    required: false
-  },   
-
   password: {
     type: String,
     required: true
+  },
+
+  email: {
+    type: String,
+    required: false
   },
 
   salt: String,
